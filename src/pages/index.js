578aa931198c65de90357d0bcf190cd193d65846
Jesus Valero <valerojesusg@gmail.com>
2022-08-18 18:55:33 -0400

    added loading spinner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Client from '../components/Client';
+import Loading from '../components/Loading';
 
 const Index = () => {
   const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getCustomers = async () => {
@@ -14,6 +16,8 @@ const Index = () => {
       } catch (error) {
         console.log(error);
       }
+
+      setLoading(false);
     };
 
     getCustomers();
@@ -38,30 +42,31 @@ const Index = () => {
   };
 
   return (
-    <>
-      <h1 className="font-bold text-4xl text-blue-900">Customers</h1>
-      <p className="mt-3">Manage your clients.</p>
+    loading ? <Loading /> :
+      <>
+        <h1 className="font-bold text-4xl text-blue-900">Customers</h1>
+        <p className="mt-3">Manage your clients.</p>
 
-      <table className="w-full mt-10 table-auto shadow bg-white">
-        <thead className="bg-blue-800 text-white">
-          <tr>
-            <th className="p-2">Name</th>
-            <th className="p-2">Contact</th>
-            <th className="p-2">Company</th>
-            <th className="p-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map(client => (
-            <Client
-              key={client.id}
-              client={client}
-              handleDelete={handleDelete}
-            />
-          ))}
-        </tbody>
-      </table>
-    </>
+        <table className="w-full mt-10 table-auto shadow bg-white">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Name</th>
+              <th className="p-2">Contact</th>
+              <th className="p-2">Company</th>
+              <th className="p-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map(client => (
+              <Client
+                key={client.id}
+                client={client}
+                handleDelete={handleDelete}
+              />
+            ))}
+          </tbody>
+        </table>
+      </>
   );
 };
 
